8a00a18a374503d8452832acd6100453d11b5a1b
Amadeo García Torrano <amadeogarcia@users.noreply.github.com>
2020-11-03 17:18:05 -0300

    Remove and update weather scripts
    Got rid of the bash script, translating it to DOS, so I won't have to call it from a `.bat` file and wait to the Linux Subsystem to boot. This will speed up the execution.

--- shell/clima.bat
+++ shell/clima.bat
@@ -1,4 +1,12 @@
-bash /mnt/c/users/amade/documents/github/proyecto-comunicaciones/shell/clima.sh
-del C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\clima\rosario.html
-copy C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\.httrack\wttr.in\Rosario*.html C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\clima
-ren C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\clima\Rosario*.html rosario.html
+cd "C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\.httrack"
+httrack "wttr.in/Rosario?0Fp" -qiC2I0H0Qu1%n%s -*.png -*/adobe-fonts.github.io/*
+xcopy Rosariob8a6.html C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\clima\rosario.html /Y
+
+:: -q   -> Do not ask questions;
+:: -iC2 -> Update a mirror in the current directory;
+:: -I0  -> Prevents the creation of an index of the sites mirrored;
+:: -H0  -> Never abandon the download from a host to be abandoned;
+:: -Q   -> Disables log files (not sure this works...);
+:: -u1  -> Check all file types except directories;
+:: -%n  -> Do not re-download locally erased files;
+:: -%s  -> Various hacks to limit re-transfers when updating.
