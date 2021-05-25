473d2ae89fb8369e91cb8e776cbd6cb72d15acec
Amadeo García Torrano <42990927+amadeogarcia@users.noreply.github.com>
2021-05-25 19:29:08 -0300

    hstart bad

--- shell/main.bat
+++ shell/main.bat
@@ -1,5 +1,4 @@
-hstart64 /NOWINDOW /NOCONSOLE /UAC /D="C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell" "clima.bat" "covid.bat" "winscp.com /script="server.bat""
-::call "C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\clima.bat"
-::call "C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\covid.bat"
-::winscp.com /script="C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\server.bat"
+call "C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\clima.bat"
+call "C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\covid.bat"
+winscp.com /script="C:\Users\amade\Documents\GitHub\proyecto-comunicaciones\shell\server.bat"
 exit
